623b273b5eee9e07d5c678f181d2cdd9476d3816
Brooke Balza <brooke.balza@commusoft.co.uk>
2017-02-14 14:45:48 +0000

    Committing

---
+++ src/components/todo/TodoItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {partial} from '../../lib/utils';
+
+export const TodoItem = (props) => {
+	const handleToggle = partial(props.handleToggle, props.id);
+	const handleRemove = partial(props.handleRemove, props.id);
+
+	return (
+		<li>
+			<span className="delete-item">
+				<a href="#" onClick={handleRemove}>{'X'}</a>
+			</span>
+		  <input
+		    type="checkbox"
+		    checked={props.isComplete}
+		    onChange={handleToggle}
+		   />
+		  {props.name}
+		</li>
+	);
+}
+
+TodoItem.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	isComplete: React.PropTypes.bool,
+	id: React.PropTypes.number.isRequired
+}
---
+++ src/components/todo/TodoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {TodoItem} from './TodoItem';
+
+export const TodoList = (props) => {
+	return (
+		<div className="todo-list">
+		  <ul>
+		    {props.todos.map((todo) => {
+		    	return (
+		    		<TodoItem
+		    			handleToggle={props.handleToggle}
+		    			key={todo.id}
+		    			{...todo}
+		    			handleRemove={props.handleRemove}
+		    		/>
+		    	);
+		    })}
+		  </ul>
+		</div>
+	);
+};
+
+TodoList.propTypes = {
+	todos: React.PropTypes.array.isRequired
+}
